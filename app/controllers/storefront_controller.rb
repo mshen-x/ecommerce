f21feb80b8d5cb1ecaf4b20bfa49dc95d62ba247
Abio Hen <hrris2011@gmail.com>
2018-07-09 13:19:25 -0400

    day one and a half

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -4,8 +4,14 @@ class StorefrontController < ApplicationController
   end
 
   def items_by_category
+  	@category = Category.find(params[:category_id])
+  	@products = @category.products
   end
 
-  def items_by_brand
+  def items_by_brand  	
+  	@products = Product.where(brand: params[:brand])
+  	@brand = params[:brand]
   end
+
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_130237) do
+ActiveRecord::Schema.define(version: 2018_07_09_164614) do
 
   create_table "catagories", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,25 @@ ActiveRecord::Schema.define(version: 2018_07_09_130237) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "quantity"
+    t.decimal "line_item_total"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "subtotal"
+    t.decimal "sales_tax"
+    t.decimal "grand_total"
+    t.integer "user_id"
+    t.text "order_items"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
